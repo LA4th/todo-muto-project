f44f36419eb7d70e7f1f0e71415ee2d733fa43de
LA4th <lorenzolaforteza311@gmail.com>
2025-08-07 21:39:26 +0800

    need to debug, the md: in li

--- src/eventListener.js
+++ src/eventListener.js
@@ -1,23 +1,28 @@
-const burgerBtn = document.getElementById("burgerBtn");
-const closeBtn = document.getElementById("closeBtn");
-const navMenu = document.getElementById("navMenu");
+const burgerBtn = document.querySelector('#burgerBtn');
+const closeBtn = document.querySelector('#closeBtn');
+const navMenu = document.querySelector('#navMenu');
+const footerContainer = document.querySelector('#footerContainer');
 
 export const eventListener = () => {
-  burgerBtn.addEventListener("click", () => {
-  navMenu.classList.remove("translate-x-full", "opacity-0", "pointer-events-none");
-});
 
-closeBtn.addEventListener("click", () => {
-  navMenu.classList.add("translate-x-full", "opacity-0", "pointer-events-none");
-});
+  burgerBtn.addEventListener('click', () => {
+    navMenu.classList.remove('translate-x-full', 'opacity-0', 'pointer-events-none');
+    footerContainer.classList.remove('translate-x-full', 'opacity-0', 'pointer-events-none');
+    burgerBtn.classList.add('hidden');
+  });
+  closeBtn.addEventListener('click', () => {
+    navMenu.classList.add('translate-x-full', 'opacity-0', 'pointer-events-none');
+    footerContainer.classList.add('translate-x-full', 'opacity-0', 'pointer-events-none');
+    burgerBtn.classList.remove('hidden');
+  });
 
 // Optional: Auto-close on mobile when a nav link is clicked
-const navLinks = navMenu.querySelectorAll("a");
+/* const navLinks = navMenu.querySelectorAll("a");
 navLinks.forEach(link => {
   link.addEventListener("click", () => {
     if (window.innerWidth < 768) {
       navMenu.classList.add("translate-x-full", "opacity-0", "pointer-events-none");
     }
   });
-});
+}); */
 };
